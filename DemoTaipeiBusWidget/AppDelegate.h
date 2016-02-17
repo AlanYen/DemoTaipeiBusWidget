9b152773174b216cb2846e851f9472c14307fd96
AlanYen <alanyen.mobile@gmail.com>
2016-02-17 15:44:12 +0800

    Initial Commit

---
+++ DemoTaipeiBusWidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTaipeiBusWidget
+//
+//  Created by AlanYen on 2016/2/17.
+//  Copyright © 2016年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTaipeiBusWidget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTaipeiBusWidget
+//
+//  Created by AlanYen on 2016/2/17.
+//  Copyright © 2016年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
